2e40bb9f371d9524c924a7e7447123bb3beaf3f7
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-03-28 16:27:45 +0000

    Agregue clase derivada Revista

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,9 +1,25 @@
 package miPrincipal;
+import java.util.Date;
 
 public class Principal {
     
 
     public static void main(String[] args) {
+        //crear objetos Publicacion
+        Publicacion publicacion = new Publicacion();
+        Date fecha = new Date("01/01/1989");
+
+        Publicacion publicacion2 = new Publicacion("McGrawnHill",fecha);
+
+        publicacion.setEditorial("Cielo");
+        Date fecha2 = new Date("12/10/2000");
+        publicacion.setFecha(fecha2);
+
+        //crear objetos Revista
+
+       
+
+
         
     }
 }
--- miPrincipal/Revista.java
+++ miPrincipal/Revista.java
@@ -1,4 +1,43 @@
 package miPrincipal;
+import java.util.Date;
 public class Revista extends Publicacion{
+     //atributos
+     private String periodicidad;
+     private int ejemplares;
+     //constructors
+     public Revista(){
+         super();
+     }
+     public Revista(String periodicidad, int ejemplares){
+         super("sin editorial",null);
+         this.periodicidad = periodicidad;
+         this.ejemplares = ejemplares;
+ 
+     }
+     public Revista(String editorial, Date fecha, String periodicidad, int ejemplares){
+         super(editorial, fecha);
+         this.periodicidad = periodicidad;
+         this.ejemplares = ejemplares;
+ 
+     }
+     //getter y setter
+     public String getPeriodicidad(){
+        return this.periodicidad;
+     }
+     public void setPeriodicidad(String periodicidad){
+        this.periodicidad =periodicidad;
+     }
+
+     public int getEjemplares(){
+        return this.ejemplares;
+     }
+     public void setEjemplares(int ejemplares){
+        this.ejemplares = ejemplares;
+     }
+
+     @Override
+     public String toString(){
+        return " Periodicidad: "+this.periodicidad+" Ejemplares: "+ejemplares;
+     }
 
 }
