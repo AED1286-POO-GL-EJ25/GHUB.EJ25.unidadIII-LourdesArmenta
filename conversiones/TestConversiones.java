b43128a3a703470b7e8811c51d659108f8e18fbf
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-04-02 17:03:22 +0000

    conversiones entre clases

---
+++ conversiones/TestConversiones.java
@@ -0,0 +1,24 @@
+package conversiones;
+
+public class TestConversiones {
+    public static void main(String[] args) {
+        int k = (int)5.8; //convierte un double a un int
+        System.out.println(k);
+
+        Integer g = new Integer(66);
+        System.out.println(g);
+
+        double x = (double)g;
+        System.out.println(x);
+
+        //Revista pb = (Revista)g; //no todas las conversiones son posibles
+
+
+
+
+        
+
+        
+    }
+    
+}
